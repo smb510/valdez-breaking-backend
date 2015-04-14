ab2356fb94b5ec937549f2df75d8294b8bbdb2d3
Scottie Biddle <smb510@gmail.com>
2015-04-13 22:36:01 -0700

    remove length on bigint

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,14 +2,14 @@
 
 # --- !Ups
 
-CREATE TABLE STORIES (
-    id bigint(20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE STORIES ()
+    id bigint NOT NULL AUTO_INCREMENT,
     eventType varchar(255) NOT NULL,
     eventBody varchar(2000) NOT NULL UNIQUE,
     importDate DATE NOT NULL,
     isBroadcast boolean NOT NULL,
     PRIMARY KEY (id)
-);
+    );
 
 # --- !Downs
 
