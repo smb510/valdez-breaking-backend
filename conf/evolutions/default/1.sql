f9af1762df36a021000be1d40aea385e5aaa1f8e
Scottie Biddle <smb510@gmail.com>
2015-04-13 22:45:22 -0700

    redo sql again

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,14 +2,14 @@
 
 # --- !Ups
 
-CREATE TABLE STORIES ()
+CREATE TABLE STORIES (
     id bigint NOT NULL AUTO_INCREMENT,
     eventType varchar(255) NOT NULL,
     eventBody varchar(2000) NOT NULL UNIQUE,
     importDate DATE NOT NULL,
     isBroadcast boolean NOT NULL,
     PRIMARY KEY (id)
-    );
+);
 
 # --- !Downs
 
